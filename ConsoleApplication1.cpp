feacf5caadf17acdf4018ce18e405d6fd27edb3f
RyzhenkoVR <115408602+RyzhenkoVR@users.noreply.github.com>
2022-10-09 17:51:50 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿// Ryzhenko V.R.
+// task 1
+// 09.10.2022
+#include <iostream>
+using namespace std;
+int main()
+{
+	int w;
+	cin >> w;
+	if (w % 2 == 0)
+	{
+		cout << "YES";
+	} 
+	else
+	{
+		cout << "NO";
+	}
+}
